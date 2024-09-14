21932426ddd93fc0c212c84fd7cfedd4f81de01e
DennisFang <dennis.fang@wtmec.com>
2024-09-14 13:28:06 +0800

    first commit

---
+++ flask_test.py
@@ -0,0 +1,20 @@
+from crypt import methods
+
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello Python"
+
+@app.route("/abc")
+def hello2():
+    return "Hello Python2"
+
+@app.route("/abc", methods = ["POST"])
+def hello3():
+    return "Hello Python3"
+
+if __name__ == "__main__":
+    app.run()
